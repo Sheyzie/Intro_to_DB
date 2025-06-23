c49b3c6a14fea7faa22b14708bd622203ca67cee
Sheyzie <oluwaseyiolukosi7@gmail.com>
2025-06-23 17:01:53 +0100

    Added task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,4 @@
 -- Print the full description of the table `books` in the specified database
--- mysql -u username -p alx_book_store < task_4.sql
 
 SELECT 
     COLUMN_NAME, 
@@ -9,6 +8,6 @@ SELECT
     COLUMN_DEFAULT, 
     EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
+WHERE TABLE_SCHEMA = 'alx_book_store'
 AND TABLE_NAME = 'books'
 ORDER BY ORDINAL_POSITION;
