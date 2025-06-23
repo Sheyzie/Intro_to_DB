185673747c2c4237cf7ac43c1f5266765505792e
Sheyzie <oluwaseyiolukosi7@gmail.com>
2025-06-23 16:43:03 +0100

    Created a database connection

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+try:
+  # Load environment variables from .env file
+  load_dotenv()
+  db_password = os.getenv('DB_PASSWORD')
+
+  connection = mysql.connector.connect(host='localhost', user='root', password=db_password)
+
+  # check if there is connection
+  if connection.is_connected():
+
+    # create a cursor
+    cursor = connection.cursor()
+
+    # select the database
+    cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store;')
+
+    print("Database 'alx_book_store' created successfully")
+  
+# catch errors
+except Error as e:
+  print('Error connecting to database', e)
+
+finally:
+  if connection.is_connected():
+    cursor.close()
+    connection.close()
+    print('Closed connection to database')
